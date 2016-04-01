e250ae42c8730d08cab2cfe427d41810a9e85a6f
maliaoreta <maliaoreta@gmail.com>
2016-03-31 19:52:49 -1000

    writes for loops, curr and next variables

--- public/js/selection-sort.js
+++ public/js/selection-sort.js
@@ -1,5 +1,18 @@
-function selectionSort () {
+function selectionSort (arr) {
 
+  for (var i = 0; i < arr.length-1; i++) {
 
-  
+    for (var k = 1; k < arr.length; k++) {
+
+      var curr = arr[i];
+      var next = arr[k];
+    
+    }
+  }
+
+  return arr;
 }
+
+
+var testArr = [9, 4, 5, 2, 1, 6, 7, 10, 3, 0, 8];
+console.log(selectionSort(testArr));
