1558f9bda0454cd6c95ba48b6311a403e38f776b
maliaoreta <maliaoreta@gmail.com>
2016-03-31 18:50:37 -1000

    tests selectionSort to check if returning an array

--- public/js/tests/selection-sort.spec.js
+++ public/js/tests/selection-sort.spec.js
@@ -3,5 +3,10 @@ describe('selectionSort', function () {
   it('should be a function', function () {
 
     expect(selectionSort).to.be.a('function');
-  })
-})
+  });
+
+  it('should return an array', function () {
+
+    expect(selectionSort([1, 2, 3])).to.be.an('array');
+  });
+});
